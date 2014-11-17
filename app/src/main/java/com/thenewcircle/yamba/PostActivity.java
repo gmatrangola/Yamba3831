9cc17ec66a2097b7880f52f3aff72546241fe8fd
Geoff Matrangola <geoff@matrangola.com>
2014-11-17 14:21:03 -0500

    Added color change.

--- app/src/main/java/com/thenewcircle/yamba/PostActivity.java
+++ app/src/main/java/com/thenewcircle/yamba/PostActivity.java
@@ -1,23 +1,32 @@
 package com.thenewcircle.yamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class PostActivity extends Activity {
+public class PostActivity extends Activity implements TextWatcher {
 
     private static final String TAG = "yamba." + PostActivity.class.getSimpleName();
     private EditText messageText;
+    private TextView charCountText;
+    private int maxChars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String maxCharsString = getResources().getString(R.string.maxChars);
+        maxChars = Integer.parseInt(maxCharsString);
+
         setContentView(R.layout.activity_post);
         messageText = (EditText) findViewById(R.id.messageText);
         Button postButton = (Button) findViewById(R.id.postButton);
@@ -27,6 +36,9 @@ public class PostActivity extends Activity {
                 Log.d(TAG, "Message = " + messageText.getText().toString());
             }
         });
+        charCountText = (TextView) findViewById(R.id.charCountText);
+
+        messageText.addTextChangedListener(this);
     }
 
 
@@ -50,5 +62,27 @@ public class PostActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        int charsRemaining = maxChars - s.length();
+        charCountText.setText(charsRemaining + "");
+        if(charsRemaining < 0) {
+            charCountText.setTextColor(getResources().getColor(R.color.error));
+        }
+        else {
+            charCountText.setTextColor(getResources().getColor(R.color.valid));
+        }
     }
 }
